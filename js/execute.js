d92ce60560739e1b37a49d6d7002fd3c67685fbb
Svetislav Grujic <svetislav.grujic@gmail.com>
2017-12-26 17:40:34 +0100

    Sprečeno kočenje skripte u slučaju kada je signal sa objekta mali (teži nuli) pa vrednost za vreme ekspozicije teži beskonačnosti.

--- js/execute.js
+++ js/execute.js
@@ -1,12 +1,14 @@
     execute: function() {
       this.getFnValues();
-      this.calculateExposure();
-      this.resetGraph();
-      this.drawGraphLines();
-      this.addGraphValues();
-      this.drawGraph();
-      this.drawHelpLines();
-      this.fillData();
-      this.logger();
-      this.result.classList.remove("collapsed");
+      if (this.eqParams.sig > 0.01) { // da bi sprečili kočenje skripte za premale vrednosti Ssig 
+        this.calculateExposure();
+        this.resetGraph();
+        this.drawGraphLines();
+        this.addGraphValues();
+        this.drawGraph();
+        this.drawHelpLines();
+        this.fillData();
+        this.logger();
+        this.result.classList.remove("collapsed");
+      }
     },
